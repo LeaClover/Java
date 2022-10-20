381d2c86e0fbd1d352275fbceb8bbd9256a98146
LeaClover <plesen_14@mail.ru>
2022-10-20 12:00:19 +0300

    add new project lesson1 task2

---
+++ Lesson1/task2.java
@@ -0,0 +1,36 @@
+﻿import java.util.Scanner;
+import java.time.LocalDateTime;
+
+// В зависимости от текущего времени вызывать сообщение 
+//доброе утро, добрый день, добрый вечер или доброй ночи
+
+public class task2 {
+
+    LocalDateTime ldt = LocalDateTime.now();
+    Integer currentTime = ldt.getHour();
+
+    public static void main(String[] args) {
+        task2 task2 = new task2();
+        Scanner iScanner = new Scanner(System.in);
+        System.out.printf("Введите имя: ");
+        String name = iScanner.next();
+        iScanner.close();
+        task2.getMSGtime(name);
+        System.out.printf("Текущее время: %s", task2.currentTime);
+    }
+
+    public void getMSGtime(String name) {
+        if (currentTime >= 5 && currentTime < 11) {
+            System.out.printf("Доброе утро, %s!\n", name);
+            }
+        if (currentTime >= 11 && currentTime < 16) {
+            System.out.printf("Добрый день, %s!\n", name);
+            }
+        if (currentTime >= 16 && currentTime < 23) {
+            System.out.printf("Добрый вечер, %s!\n", name);
+            }
+        if (currentTime >= 23 && currentTime < 5) {
+            System.out.printf("Доброй ночи, %s!\n", name);
+            }
+    }
+}
