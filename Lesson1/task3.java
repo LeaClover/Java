b9b63b7c2077ba665cd5abcc01955655efb21d19
LeaClover <plesen_14@mail.ru>
2022-10-20 12:00:25 +0300

    add new project lesson1 task3

---
+++ Lesson1/task3.java
@@ -0,0 +1,27 @@
+﻿// Дан массив двоичных чисел, например [1,1,0,1,0]
+// Вывести максимальное количесвто идущих подряд единиц
+
+public class task3 {
+    public static void main(String[] args) {
+        int [] arr = {1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 0};
+        task3 task3 = new task3();
+        int max = task3.getMaxCount(arr);
+        System.out.printf("Максимальное количество '1' подряд: %d", max);
+    }
+
+    public int getMaxCount(int [] arr) {
+        int count = 0;
+        int max = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == 1) count++;
+            else {
+                if (count != 0) {
+                    if (max < count) max = count;
+                    count = 0;
+                }
+            }
+            if (count > max) max = count;
+        }
+        return max;
+    }
+}
