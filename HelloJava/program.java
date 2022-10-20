440e283b35d852c9c48ac86b20307132e794cdf5
LeaClover <plesen_14@mail.ru>
2022-10-20 11:59:15 +0300

    add new project hello java

---
+++ HelloJava/program.java
@@ -0,0 +1,15 @@
+﻿import java.util.Scanner;
+
+public class program {
+    public static void main(String[] args) {
+        System.out.println("hello world!");
+        int a = 123;
+        a = a-- - --a;
+        System.out.println(a);
+        Scanner iScanner = new Scanner(System.in);
+        System.out.printf("name: ");
+        String name = iScanner.nextLine();
+        System.out.printf("Привет, %s!", name);
+        iScanner.close();
+    }
+}
