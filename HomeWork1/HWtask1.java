020e16ad5b777aaa109ab144ff193e0ea4a96927
LeaClover <plesen_14@mail.ru>
2022-10-20 11:59:51 +0300

    add new project hw1 task1

---
+++ HomeWork1/HWtask1.java
@@ -0,0 +1,25 @@
+﻿import java.util.Scanner;
+import java.lang.Math;
+// Реализовать функции возведения числа a в степень b
+
+public class HWtask1 {
+    public static void main(String[] args) {
+        Scanner iScanner = new Scanner(System.in);
+        HWtask1 hw = new HWtask1();
+        System.out.printf("Введите число a: ");
+        int a = Integer.parseInt(iScanner.nextLine());
+        System.out.printf("Введите число b: ");
+        double b = Double.parseDouble(iScanner.nextLine());
+        double result = hw.getPower(a, b);
+        hw.printResult(a, b, result);
+        iScanner.close();
+    }
+
+    public double getPower(int a, double b) {
+        return Math.pow(a, b);
+    }
+
+    public void printResult(int a, double b, double result) {
+        System.out.printf("%d ^ %.2f = %.2f", a, b, result);
+    }
+}
