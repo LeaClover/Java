7226f7ed97d05d5eb167662fd6b2252547d76f01
LeaClover <plesen_14@mail.ru>
2022-10-20 12:00:13 +0300

    add new project lesson1 task1

---
+++ Lesson1/task1.java
@@ -0,0 +1,19 @@
+﻿import java.util.Scanner;
+
+//Написать программу, которая запросит пользователя ввести <Имя> в консоли.
+//Получит введенную строку и выведет в консоль сообщение “Привет, <Имя>!”
+
+public class task1 {
+    public static void main(String[] args) {
+        Scanner iScanner = new Scanner(System.in);
+        System.out.printf("Введите имя: ");
+        String name = iScanner.nextLine();
+        task1 task1 = new task1();
+        task1.getMSG(name);
+        iScanner.close();
+    }
+
+    public void getMSG(String name) {
+        System.out.printf("Привет, %s!", name);
+    }
+}
